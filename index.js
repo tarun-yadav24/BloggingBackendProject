755348a09ca5df6aec6f618ebc2b24859d6264f6
tarun-yadav24 <tarun.k.yadav24@gmail.com>
2024-09-10 11:49:39 +0530

    Blogging project Backend

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let userData = []
+let Id = 1
+
+app.get('/posts',(req,res)=>{
+    res.status(201).send(userData)
+})
+
+app.get('/posts/:id',(req,res)=>{
+    const userId = userData.find(U=>U.Id === parseInt(req.params.id))
+    if(!userId){
+        return res.status(404).send('User Not found')
+    }
+    res.status(201).send(userId)
+})
+//adding
+app.post('/posts',(req,res)=>{
+    const {title,author,content,publication_date} = req.body
+    const userAdd = {Id:Id++,title,author,content,publication_date}
+    userData.push(userAdd)
+    res.status(201).send(userAdd)
+})
+
+
+app.put('/posts/:id',(req,res)=>{
+    const userId = userData.find(U=>U.Id === parseInt(req.params.id))
+    const {title,author,content,publication_date} = req.body
+    userId.title = title
+    userId.author = author
+    userId.content = content
+    userId.publication_date = publication_date
+    res.status(200).send(userId)
+})
+
+app.delete('/posts/:id',(req,res)=>{
+    const index = userData.findIndex(i=>i.Id === parseInt(req.params.id))
+    if(index === -1 ){
+        return res.status(404).send('User Not found')
+    }
+    userData.splice(index,1)
+    res.status(200).send('Deleted')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening at port ${port}`)
+})
